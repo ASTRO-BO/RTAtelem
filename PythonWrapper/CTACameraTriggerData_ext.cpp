765b6a530dcb5cc71d2d2a04ed37d5690739981f
Valentina Fioretti <fioretti@icedevremote.(none)>
2014-01-31 11:44:05 +0100

    Python wrapper makefile updated

--- PythonWrapper/CTACameraTriggerData_ext.cpp
+++ PythonWrapper/CTACameraTriggerData_ext.cpp
@@ -18,6 +18,7 @@
 #include <boost/python/module.hpp>
 #include <boost/python/class.hpp>
 #include <boost/python/scope.hpp>
+#include <boost/shared_ptr.hpp>
 #include <CTACameraTriggerData.cpp>
 #include <CTACamera.cpp>
 #include <CTAPacket.cpp>
@@ -27,14 +28,14 @@
 using namespace boost::python;
 
 //buffer wrapper
-static object CTAPacket_readPacket(RTATelem::CTAPacket& self) {
-  unsigned char* buffer = self.readPacket();
-  int size = self.getInputPacketDimension(buffer);
+//static object CTAPacket_readPacket(RTATelem::CTAPacket& self) {
+//  unsigned char* buffer = self.readPacket();
+//  int size = self.getInputPacketDimension(buffer);
 
-  PyObject* py_buf = PyBuffer_FromMemory(buffer, size);
-  object retval = object(handle<>(py_buf));
-  return retval;
-}
+//  PyObject* py_buf = PyBuffer_FromMemory(buffer, size);
+//  object retval = object(handle<>(py_buf));
+//  return retval;
+//}
 
 class DummyCTA{};
 
@@ -47,20 +48,20 @@ BOOST_PYTHON_MODULE(CTACameraTriggerData_ext)
 
          class_<RTATelem::CTAPacketHeader>("CTAPacketHeader", no_init)
               .def("setSSC", &RTATelem::CTAPacketHeader::setSSC)
-              .def("getSSC", &RTATelem::CTAPacketHeader::getSSC)
+              //.def("getSSC", &RTATelem::CTAPacketHeader::getSSC) GET METHODS
               .def("setAPID", &RTATelem::CTAPacketHeader::setAPID)
-              .def("getAPID", &RTATelem::CTAPacketHeader::getAPID)
+              //.def("getAPID", &RTATelem::CTAPacketHeader::getAPID) GET METHODS
               .def("setMetadata", &RTATelem::CTAPacketHeader::setMetadata)
-              .def("getMetadata", &RTATelem::CTAPacketHeader::getMetadata)
+              //.def("getMetadata", &RTATelem::CTAPacketHeader::getMetadata) GET METHODS
               .def("setTime", &RTATelem::CTAPacketHeader::setTime)
-              .def("getTime", &RTATelem::CTAPacketHeader::getTime)
+              //.def("getTime", &RTATelem::CTAPacketHeader::getTime) GET METHODS
               ;
 
          class_<RTATelem::CTAPacket>("CTAPacket", no_init)
               //.def(init<std::string, std::string, std::string>())
               .add_property("header",make_getter(&RTATelem::CTAPacket::header, return_value_policy<reference_existing_object>()),make_setter(&RTATelem::CTAPacket::header,return_value_policy<reference_existing_object>()))
               .def("writePacket", &RTATelem::CTAPacket::writePacket)
-              .def("readPacket", &CTAPacket_readPacket, (boost::python::arg("self")), "Get buffer")
+              //.def("readPacket", &CTAPacket_readPacket, (boost::python::arg("self")), "Get buffer")
               .def("readPacketPy", &RTATelem::CTAPacket::readPacketPy)
               .def("printPacket_output", &RTATelem::CTAPacket::printPacket_output)
               //.def("printPacket_input", &RTATelem::CTAPacket::printPacket_input)
@@ -69,31 +70,31 @@ BOOST_PYTHON_MODULE(CTACameraTriggerData_ext)
          class_<RTATelem::CTACamera, bases<RTATelem::CTAPacket> >("CTACamera", no_init)
               //.def(init<std::string, std::string, std::string>())
               .def("setNumberOfPixels", &RTATelem::CTACamera::setNumberOfPixels)
-              .def("getNumberOfPixels", &RTATelem::CTACamera::getNumberOfPixels)
+              //.def("getNumberOfPixels", &RTATelem::CTACamera::getNumberOfPixels) GET METHODS
               ;               
          
          // Define a class A in the current scope, a
          class_<RTATelem::CTACameraTriggerData, bases<RTATelem::CTACamera> >("CTACameraTriggerData", init<std::string>())
               .def(init<std::string, std::string, std::string>())
               .def("setTelescopeId", &RTATelem::CTACameraTriggerData::setTelescopeId)
-              .def("getTelescopeId", &RTATelem::CTACameraTriggerData::getTelescopeId)
+              //.def("getTelescopeId", &RTATelem::CTACameraTriggerData::getTelescopeId) GET METHODS
               .def("setEventNumber", &RTATelem::CTACameraTriggerData::setEventNumber)
-              .def("getEventNumber", &RTATelem::CTACameraTriggerData::getEventNumber)
+              //.def("getEventNumber", &RTATelem::CTACameraTriggerData::getEventNumber) GET METHODS
               .def("setNumberOfTriggeredTelescopes", &RTATelem::CTACameraTriggerData::setNumberOfTriggeredTelescopes)
-              .def("getNumberOfTriggeredTelescopes", &RTATelem::CTACameraTriggerData::getNumberOfTriggeredTelescopes)
+              //.def("getNumberOfTriggeredTelescopes", &RTATelem::CTACameraTriggerData::getNumberOfTriggeredTelescopes) GET METHODS
               .def("setIndexOfCurrentTriggeredTelescope", &RTATelem::CTACameraTriggerData::setIndexOfCurrentTriggeredTelescope)
-              .def("getIndexOfCurrentTriggeredTelescope", &RTATelem::CTACameraTriggerData::getIndexOfCurrentTriggeredTelescope)
+              //.def("getIndexOfCurrentTriggeredTelescope", &RTATelem::CTACameraTriggerData::getIndexOfCurrentTriggeredTelescope) GET METHODS
               .def("setPixelId", &RTATelem::CTACameraTriggerData::setPixelId)
-              .def("getPixelId", &RTATelem::CTACameraTriggerData::getPixelId)
+              //.def("getPixelId", &RTATelem::CTACameraTriggerData::getPixelId) GET METHODS
               .def("setNumberOfSamples", &RTATelem::CTACameraTriggerData::setNumberOfSamples)
-              .def("getNumberOfSamples", &RTATelem::CTACameraTriggerData::getNumberOfSamples)
+              //.def("getNumberOfSamples", &RTATelem::CTACameraTriggerData::getNumberOfSamples) GET METHODS
               .def("setSampleValue", &RTATelem::CTACameraTriggerData::setSampleValue)
-              .def("getSampleValue", &RTATelem::CTACameraTriggerData::getSampleValue)
-              .def("getPixelData", &RTATelem::CTACameraTriggerData::getPixelData, return_value_policy<manage_new_object>())
+              //.def("getSampleValue", &RTATelem::CTACameraTriggerData::getSampleValue) GET METHODS
+              //.def("getPixelData", &RTATelem::CTACameraTriggerData::getPixelData, return_value_policy<manage_new_object>()) GET METHODS
               ;
 
         // ByteStream definition
-        scope PacketLib = class_<DummyCTA>("PacketLib");
-        class_<PacketLib::ByteStream >("ByteStream", no_init)
-            .def("getByte", &PacketLib::ByteStream::getByte);
+       // scope PacketLib = class_<DummyCTA>("PacketLib");
+       // class_<PacketLib::ByteStream >("ByteStream", no_init)
+       //     .def("getByte", &PacketLib::ByteStream::getByte);
 }
