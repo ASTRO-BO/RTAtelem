b7b80355537b0a92aaf1269b552ea0a7333dc1e5
Andrea Bulgarelli <bulgarelli@iasfbo.inaf.it>
2014-02-28 16:36:13 +0100

    CTACameraTriggerData1

---
+++ code/CTACameraTriggerData1.cpp
@@ -0,0 +1,146 @@
+/***************************************************************************
+                          CTACameraTriggerData.cpp  -  description
+                             -------------------
+    copyright            : (C) 2013 Andrea Bulgarelli
+    email                : bulgarelli@iasfbo.inaf.it
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software for non commercial purpose              *
+ *   and for public research institutes; you can redistribute it and/or    *
+ *   modify it under the terms of the GNU General Public License.          *
+ *   For commercial purpose see appropriate license terms                  *
+ *                                                                         *
+ ***************************************************************************/
+
+
+#include "CTACameraTriggerData.h"
+#include <packet/OutputFile.h>
+#include <packet/InputFile.h>
+#include <packet/PacketDataField.h>
+#include <packet/SDFRBlock.h>
+#include <packet/OutputPacketStream.h>
+
+#define RBLOCK_TELESCOPE 0
+#define RBLOCK_PIXEL 0
+#define RBLOCK_PIXELID 1
+#define RBLOCK_SAMPLE 0
+
+RTATelem::CTACameraTriggerData::CTACameraTriggerData(string packetConfig, string tmInputFileName, string tmOutputFileName) : CTACamera(packetConfig, tmInputFileName, tmOutputFileName) {
+
+}
+
+RTATelem::CTACameraTriggerData::CTACameraTriggerData(string packetConfig) : CTACamera(packetConfig) {
+
+
+}
+
+
+void RTATelem::CTACameraTriggerData::setTelescopeId(word telescopeID) {
+    /// Get a pointer to the source data field
+    SDFRBlock* sdf = (SDFRBlock*) outputPacket->dataField->sourceDataField;
+    sdf->setFieldValue(2, telescopeID);
+}
+
+word RTATelem::CTACameraTriggerData::getTelescopeId() {
+    /// Get a pointer to the source data field
+    SDFRBlock* sdf = (SDFRBlock*) inputPacket->dataField->sourceDataField;
+    return sdf->getFieldValue(2);
+}
+
+
+RTATelem::CTACameraTriggerData::~CTACameraTriggerData() {
+
+}
+
+void RTATelem::CTACameraTriggerData::setNumberOfTriggeredTelescopes(byte number) {
+    SDFRBlock* sdf = (SDFRBlock*) outputPacket->dataField->sourceDataField;
+    sdf->setFieldValue(3, (word) number);
+}
+
+byte RTATelem::CTACameraTriggerData::getNumberOfTriggeredTelescopes() {
+	SDFRBlock* sdf = (SDFRBlock*) inputPacket->dataField->sourceDataField;
+	return (byte) sdf->getFieldValue(3);
+}
+
+void RTATelem::CTACameraTriggerData::setIndexOfCurrentTriggeredTelescope(byte telescopeIndex) {
+	 SDFRBlock* sdf = (SDFRBlock*) outputPacket->dataField->sourceDataField;
+	 sdf->setFieldValue(4, (word) telescopeIndex);
+}
+
+byte RTATelem::CTACameraTriggerData::getIndexOfCurrentTriggeredTelescope() {
+	SDFRBlock* sdf = (SDFRBlock*) inputPacket->dataField->sourceDataField;
+	return (byte) sdf->getFieldValue(4);
+}
+
+
+
+void RTATelem::CTACameraTriggerData::setEventNumber(dword event) {
+	SDFRBlock* sdf = (SDFRBlock*) outputPacket->dataField->sourceDataField;
+    sdf->setFieldValue_4_14(0, event);
+}
+
+
+dword RTATelem::CTACameraTriggerData::getEventNumber() {
+	SDFRBlock* sdf = (SDFRBlock*) inputPacket->dataField->sourceDataField;
+    return sdf->getFieldValue_4_14(0);
+}
+
+void RTATelem::CTACameraTriggerData::setPixelId(word pixelIndex, word pixelID) {
+    /// Get a pointer to the source data field
+    SDFRBlock* sdf = (SDFRBlock*) outputPacket->dataField->sourceDataField;
+    SDFRBBlock* pixel = (SDFRBBlock*) sdf->getBlock(pixelIndex, RBLOCK_PIXELID);
+    pixel->setFieldValue(0, pixelID);
+}
+
+void RTATelem::CTACameraTriggerData::setNumberOfSamples(word pixelIndex, word number) {
+    /// Get a pointer to the source data field
+    SDFRBlock* sdf = (SDFRBlock*) outputPacket->dataField->sourceDataField;
+    SDFRBBlock* pixel = (SDFRBBlock*) sdf->getBlock(pixelIndex, RBLOCK_PIXEL);
+    pixel->setNumberOfRealDataBlock(number, RBLOCK_SAMPLE);
+}
+
+void RTATelem::CTACameraTriggerData::setSampleValue(word pixelIndex, word sampleIndex, word FADC) {
+    /// Get a pointer to the source data field
+    SDFRBlock* sdf = (SDFRBlock*) outputPacket->dataField->sourceDataField;
+    SDFRBBlock* pixel = (SDFRBBlock*) sdf->getBlock(pixelIndex, RBLOCK_PIXEL);
+    /// VARIABLE FORMAT
+    SDFRBBlock* sample = (SDFRBBlock*) pixel->getBlock(sampleIndex, RBLOCK_SAMPLE);
+    sample->setFieldValue(0, FADC);
+}
+
+
+word RTATelem::CTACameraTriggerData::getPixelId(word pixelIndex) {
+    /// Get a pointer to the source data field
+    SDFRBlock* sdf = (SDFRBlock*) inputPacket->dataField->sourceDataField;
+    SDFRBBlock* pixel = (SDFRBBlock*) sdf->getBlock(pixelIndex, RBLOCK_PIXELID);
+    return pixel->getFieldValue(0);
+}
+
+
+
+ByteStreamPtr RTATelem::CTACameraTriggerData::getPixelData(word pixelIndex) {
+    /// Get a pointer to the source data field
+    SDFRBlock* sdf = (SDFRBlock*) inputPacket->dataField->sourceDataField;
+    SDFRBBlock* pixel = (SDFRBBlock*) sdf->getBlock(pixelIndex, RBLOCK_PIXEL);
+    return pixel->getByteStream();
+}
+
+word RTATelem::CTACameraTriggerData::getNumberOfSamples(word pixelIndex) {
+    /// Get a pointer to the source data field
+    SDFRBlock* sdf = (SDFRBlock*) inputPacket->dataField->sourceDataField;
+    SDFRBBlock* pixel = (SDFRBBlock*) sdf->getBlock(pixelIndex, RBLOCK_PIXEL);
+    return pixel->getNumberOfRealDataBlock();
+}
+
+word RTATelem::CTACameraTriggerData::getSampleValue(word pixelIndex, word sampleIndex) {
+    /// Get a pointer to the source data field
+    SDFRBlock* sdf = (SDFRBlock*) inputPacket->dataField->sourceDataField;
+    SDFRBBlock* pixel = (SDFRBBlock*) sdf->getBlock(pixelIndex, RBLOCK_PIXEL);
+    /// VARIABLE FORMAT
+    SDFRBBlock* sample = (SDFRBBlock*) pixel->getBlock(sampleIndex, RBLOCK_SAMPLE);
+    return sample->getFieldValue(0);
+}
+
+
---
+++ code/CTACameraTriggerData1.h
@@ -0,0 +1,107 @@
+/***************************************************************************
+                          CTATriggeredTelescope.h  -  description
+                             -------------------
+    copyright            : (C) 2013 Andrea Bulgarelli
+    email                : bulgarelli@iasfbo.inaf.it
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software for non commercial purpose              *
+ *   and for public research institutes; you can redistribute it and/or    *
+ *   modify it under the terms of the GNU General Public License.          *
+ *   For commercial purpose see appropriate license terms                  *
+ *                                                                         *
+ ***************************************************************************/
+
+#ifndef _CTACAMERATRIGGERDATA_H
+#define _CTACAMERATRIGGERDATA_H
+#include "CTACamera.h"
+
+namespace RTATelem {
+
+/// Creating and writing a packet for each CTA triggered telescope,
+/// containg the FADC value at each sample of the telescope pixel
+/// \brief CTA triggered telescope packet class
+class CTACameraTriggerData : public RTATelem::CTACamera {
+
+
+public:
+    /// It takes the configuration file .stream and the input/output file name
+    /// \param packetConfig The packet configuration file (.stream)
+    /// \param tmInputFileName The input file name of the packet
+    /// \param tmOutputFileName The output file name of the packet
+	CTACameraTriggerData(string packetConfig, string tmInputFileName, string tmOutputFileName);
+
+	CTACameraTriggerData(string packetConfig);
+
+    ~CTACameraTriggerData();
+
+	/// It sets the telescope ID (identification number)
+	/// \param telescopeID The telescope unique ID
+	void setTelescopeId(word telescopeID);
+
+	/// It gets the telescope ID (identification number)
+	word getTelescopeId();
+
+	void setEventNumber(dword event);
+
+	dword getEventNumber();
+
+    /// It sets the number of triggered telescopes
+    /// \param number Number of triggered telescopes
+    void setNumberOfTriggeredTelescopes(byte number);
+
+    /// It sets the index of the telescope converted to packet
+    /// \param telescopeIndex The index of the written telescope (0..NumberOfTelescopes-1)
+    void setIndexOfCurrentTriggeredTelescope(byte telescopeIndex);
+	
+	/// It sets the pixel ID (identification number)
+    /// \param pixelIndex The index of the pixel (0..NumberOfPixels-1)
+    /// \param pixelID The pixel unique ID
+    void setPixelId(word pixelIndex, word pixelID);
+	
+    /// It gets the pixel ID (identification number)
+    word getPixelId(word pixelIndex);
+
+    /// Get the data of a pixel as stream of byte. Example about how to use this stream:
+    /// ByteStreamPtr fadc = trtel->getPixelData(0);
+	/// cout << fadc->printStreamInHexadecimal() << endl;
+    /// cout << fadc->getDimension() << endl;
+	/// fadc->swapWordForIntel();
+	/// word *c = (word*) fadc->stream;
+	/// cout << "pixel id " << c[0] << endl;
+	/// cout << "number of slices " << c[1] << endl;
+	/// cout << "value of first slice " << c[2] << endl;
+    /// \param The index of the pixel
+    /// \return ByteStream of the pixel.
+    ByteStreamPtr getPixelData(word pixelIndex);
+
+    /// It sets the number of samples
+    /// \param pixelIndex The index of the pixel (0..NumberOfPixels-1)
+    /// \param number The number of samples of the pixel pixelIndex
+    void setNumberOfSamples(word pixelIndex, word number);
+
+    /// It sets the sample FADC value
+    /// \param pixelIndex The index of the pixel (0..NumberOfPixels-1)
+    /// \param sampleIndex The index of the sample (0..NumberOfSamples-1)
+    /// \param FADC The FADC values of the sample sampleIndex of the pixel pixelIndex
+    void setSampleValue(word pixelIndex, word sampleIndex, word FADC);
+
+    /// It gets the number of triggered telescopes
+    byte getNumberOfTriggeredTelescopes();
+
+    /// It gets the index of the telescope converted to packet
+    byte getIndexOfCurrentTriggeredTelescope();
+
+    /// It gets the number of samples
+    word getNumberOfSamples(word pixelIndex);
+
+    /// It gets the sample FADC value
+    word getSampleValue(word pixelIndex, word sampleIndex);
+
+
+};
+}
+
+#endif
